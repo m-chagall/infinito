1a159a431bebccf781d976b40377c838bb752dd8
m-chagall <77046386+m-chagall@users.noreply.github.com>
2022-07-21 12:42:25 +0200

    link update a -> button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,24 +12,40 @@ export default function Footer(props) {
 
           <ul className="flex flex-nowrap items-center justify-center text-sm text-gray-800 lg:justify-end mb-0 dark:text-gray-400">
             <li>
-              <a href="/about" className="mr-4 hover:underline md:mr-6 ">
+              <button
+                onClick={() => {
+                  navigate("/about");
+                }}
+                className="mr-4 hover:underline md:mr-6 ">
                 About
-              </a>
+              </button>
             </li>
             <li>
-              <a href="/privacy" className="mr-4 hover:underline md:mr-6">
+              <button
+                onClick={() => {
+                  navigate("/privacy");
+                }}
+                className="mr-4 hover:underline md:mr-6">
                 Privacy Policy
-              </a>
+              </button>
             </li>
             <li>
-              <a href="/license" className="mr-4 hover:underline md:mr-6 ">
+              <button
+                onClick={() => {
+                  navigate("/license");
+                }}
+                className="mr-4 hover:underline md:mr-6 ">
                 Licensing
-              </a>
+              </button>
             </li>
             <li>
-              <a href="/contact" className="hover:underline">
+              <button
+                onClick={() => {
+                  navigate("/contact");
+                }}
+                className="hover:underline">
                 Contact
-              </a>
+              </button>
             </li>
           </ul>
         </div>
@@ -41,13 +57,17 @@ export default function Footer(props) {
           <br className="sm:hidden" />
           (ITALIA) Via Dante 999, Milano, Italia{" "}
         </span>
-        {/* <span className="block text-xs text-center text-gray-400 sm:text-sm sm:text-center dark:text-gray-400">사업자등록번호: 000-00-00000  <span className="hidden sm:inline">| </span> <br className="sm:hidden"/>통신판매업신고번호:제0000-서울동작-0000호 <a href="/" className="hover:underline">[정보조회]</a></span> */}
+        {/* <span className="block text-xs text-center text-gray-400 sm:text-sm sm:text-center dark:text-gray-400">사업자등록번호: 000-00-00000  <span className="hidden sm:inline">| </span> <br className="sm:hidden"/>통신판매업신고번호:제0000-서울동작-0000호 <button onClick={()=>{navigate("/" className=)}}"hover:underline">[정보조회]</button></span> */}
         {/* <span className="block text-xs text-center text-gray-400 sm:text-sm sm:text-center dark:text-gray-400">Support: help[@]domain.com <span className="hidden sm:inline">| </span> <br className="sm:hidden"/>Marketing/Partnership: marketing[@]domain.com </span> */}
         <span className="block text-xs text-center text-gray-400 font-semibold sm:text-sm sm:text-center dark:text-gray-400">
           COPYRIGHT © 2022{" "}
-          <a href="/" className="hover:underline">
+          <button
+            onClick={() => {
+              navigate("/");
+            }}
+            className="hover:underline">
             INFINITO
-          </a>
+          </button>
           . ALL RIGHTS MAY OR MAY NOT BE RESERVED :-)
         </span>
       </footer>
